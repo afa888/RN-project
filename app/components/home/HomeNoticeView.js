b1b6b1ac8a1996f329a12eae6358610befc2aed7
fa <654321yjn>
2019-11-09 11:19:41 +0700

    恶心的需求，自定义了一个轮播图组件。

--- app/components/home/HomeNoticeView.js
+++ app/components/home/HomeNoticeView.js
@@ -3,14 +3,13 @@ import {Image, StyleSheet, View, Text, Alert, DeviceEventEmitter} from "react-na
 import deviceValue from "../../utils/DeviceValue";
 import FastImage from 'react-native-fast-image'
 import {CAGENT} from "../../utils/Config";
-import {textTitleColor, theme_color, ThemeEditTextTextColor} from "../../utils/AllColor";
+import {textTitleColor, theme_color, ThemeEditTextTextColor, commonButton2BGColor} from "../../utils/AllColor";
 import http from "../../http/httpFetch";
-import Swiper from "react-native-swiper";
 import {MarqueeHorizontal, MarqueeVertical} from 'react-native-marquee-ab';
 import {getStoreData} from "../../http/AsyncStorage";
 import SplashScreen from "react-native-splash-screen";
 import type {ViewStyle} from "react-native/Libraries/StyleSheet/StyleSheet";
-
+import BetterBanner from '../../customizeview/BetterBanner'
 
 export default class HomeNoticeView extends Component<Props> {
     constructor(props) {
@@ -28,18 +27,24 @@ export default class HomeNoticeView extends Component<Props> {
 
     initView = () => {
         let iView = []
+        let iImag = [];
         for (var i = 0; i < deviceValue.imgUrl.length; i++) {
+            if (i !== deviceValue.imgUrl.length - 1) {
+                iImag.push({uri: deviceValue.imgUrl[i]},)
+            }
+
             iView.push(<View style={styles.slideFastView}><FastImage
                 key={i}
                 style={styles.slideFastImage}
                 source={{
-
+                    uri: deviceValue.imgUrl[i],
                     priority: FastImage.priority.high,
 
                 }}
-                resizeMode={FastImage.resizeMode.contain}
+                resizeMode={FastImage.resizeMode.cover}
             /></View>)
         }
+        this.setState({imageView: iImag})
         console.log('lllll轮播')
         console.log(deviceValue.imgUrl)
         this.setState({imageView: iView})
@@ -101,21 +106,6 @@ export default class HomeNoticeView extends Component<Props> {
         });
     }
 
-    /*    renderItem({item, index}) {
-            return (
-                <View style={styles.slideFastImage}>
-                    <FastImage
-                        style={styles.slideFastImage}
-                        source={{
-                            uri: item,
-                            priority: FastImage.priority.high,
-
-                        }}
-                        resizeMode={FastImage.resizeMode.cover}
-                    />
-                </View>
-            );
-        }*/
 
     renderNoticeItem({item, index}) {
         return (
@@ -131,84 +121,23 @@ export default class HomeNoticeView extends Component<Props> {
         return (<View>
             <View style={{
                 width: deviceValue.windowWidth,
-                height: (deviceValue.windowWidth - 30) * (301 / 750) + 40,
+                height: (deviceValue.windowWidth - 30) * (301 / 750) + 20 + 10,
                 backgroundColor: 'white',
                 flexDirection: 'column',
-                alignItems: 'center'
+                alignItems: 'center',
+                paddingTop: 10
             }}>
-                <View style={{
-                    width: deviceValue.windowWidth - 30,
-                    height: (deviceValue.windowWidth - 30) * (301 / 750),
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginTop: 12,
-                    backgroundColor: 'red',
-                }}>
-                    {this.state.imageView.length > 0 && <Swiper style={styles.wrapper}
-                                                                showsButtons={false}
-                                                                autoplay={true}
-                                                                showsPagination={true}
-                                                                autoplayTimeout={4}
-                                                                loadMinimal={true}
-                                                                removeClippedSubviews={false}
-                                                                dot={<View style={{
-                                                                    backgroundColor: 'rgba(0,0,0,.5)',
-                                                                    width: 8,
-                                                                    height: 8,
-                                                                    borderRadius: 4,
-                                                                    marginLeft: 3,
-                                                                    marginRight: 3,
-                                                                    marginTop: 3,
-                                                                    marginBottom: 3,
-                                                                }}/>}
-                                                                activeDot={<View style={{
-                                                                    backgroundColor: theme_color,
-                                                                    width: 8,
-                                                                    height: 8,
-                                                                    borderRadius: 4,
-                                                                    marginLeft: 3,
-                                                                    marginRight: 3,
-                                                                    marginTop: 3,
-                                                                    marginBottom: 3
-                                                                }}/>}
-                                                                paginationStyle={{
-                                                                    bottom: -20,
-                                                                }}
-                                                                onIndexChanged={(index) => {
-                                                                    console.log("轮播图位置 " + index)
-                                                                }}
-                    >
-                       {/* {this.state.imageView}*/}
-                        <View style={styles.slideFastView}><FastImage
-                            key={0}
-                            style={styles.slideFastImage}
-                            source={{
-
-                                priority: FastImage.priority.high,
-
-                            }}
-                            resizeMode={FastImage.resizeMode.contain}
-                        /></View>
-                        <View style={styles.slideFastView2}><FastImage
-                            key={0}
-                            style={styles.slideFastImage}
-                            source={{
-
-                                priority: FastImage.priority.high,
-
-                            }}
-                            resizeMode={FastImage.resizeMode.contain}
-                        /></View>
-                    </Swiper>}
-                </View>
-                <View style={{
-                    height: 3,
-                    width: deviceValue.windowWidth * (1 / 20),
-                    backgroundColor: theme_color,
-                    marginRight: 5
-                }}>
-                </View>
+                {this.state.imageView.length > 0 && <BetterBanner
+                    style={{width: deviceValue.windowWidth - 30, marginRight: 15, marginLeft: 15,}}
+                    bannerHeight={(deviceValue.windowWidth - 30) * (301 / 750) + 20}
+                    bannerComponents={this.state.imageView}
+                    onPress={(index) => {
+                    }}
+                    isSeamlessScroll={true}
+                    indicatorGroupPosition={'center'}
+                    indicatorContainerHeight={20}
+                    indicatorStyle={{indicatorWidth:12,indicatorHeight:2,indicatorColor:'red'}}
+                />}
 
             </View>
 
@@ -243,35 +172,25 @@ export default class HomeNoticeView extends Component<Props> {
 }
 const styles = StyleSheet.create({
     wrapper: {
-        width: deviceValue.windowWidth-30 ,
+        width: deviceValue.windowWidth - 30,
         height: (deviceValue.windowWidth - 30) * (301 / 750),
-        backgroundColor: 'blue',
-        marginRight:15,
-        marginLeft:15
+        marginRight: 15,
+        marginLeft: 15
 
     },
     slideFastImage: {
 
-        width: deviceValue.windowWidth-15 ,
+        width: deviceValue.windowWidth - 30,
         height: (deviceValue.windowWidth - 30) * (301 / 750),
         borderRadius: 6,
     },
     slideFastView: {
-        width: deviceValue.windowWidth-30 ,
+        width: deviceValue.windowWidth - 30,
         height: (deviceValue.windowWidth - 30) * (301 / 750),
-        flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'green',
-    },   slideFastView2: {
-        width: deviceValue.windowWidth-60 ,
-        height: (deviceValue.windowWidth - 30) * (301 / 750),
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'red',
-
-
+        marginLeft: 15,
+        marginRight: 15,
     },
     noticeView: {
         backgroundColor: 'white',
