ba7bc947bb06de5d42084ce83b0d505f0bf37b61
Nail <cnnail@yahoo.com>
2019-11-11 10:18:56 +0700

    修改关于页面的样式

--- app/components/memberCenter/help/AboutPage.js
+++ app/components/memberCenter/help/AboutPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import {
-    View, Text, Image, SafeAreaView, StyleSheet,
+    ScrollView, Text, Image, TouchableOpacity, SafeAreaView, StyleSheet,
 } from "react-native";
 import MainTheme from '../../../utils/AllColor';
 
@@ -22,25 +22,27 @@ export default class AboutPage extends Component<Props> {
     constructor(props) {
         super(props);
 
-        this.setState({
+        this.state = {
             content: '\t业精于专，功成于勤！天下网络集团，专业系统提供商、支付快充系统及API游戏供应商。 ' +
                 '我司秉承诚信、专业、勤奋、责任的企业文化，坚持技术创新、稳健务实，300+技术开发工程师致力' +
                 '于产品的创新与提升，协助客户从品牌价值出发，立足网络，将产品定位于国际市场，' +
                 '同时接入全球众多知名品牌博娱产品，为客户提供高效、稳定的系统和游戏产品。 ' +
                 '此外我们拥有最专业的技术团队为您保驾护航，让您在运营中体验到专业、稳定的天下管理系' +
                 '统及高效、贴心的售后服务。',
-        });
+        };
 
     }
 
     render() {
         return (
-            <SafeAreaView style={styles.container}>
-                <Image style={styles.logoImage}
-                    source={require('../../../static/img/UserCenter/userCenter_about_logo.png')} />
-                <Image style={styles.centerImage}
-                    source={require('../../../static/img/UserCenter/userCenter_about_content.png')}/>
-                <Text style={styles.bottomText}>{this.state.content}</Text>
+            <SafeAreaView style={{flex:1}}>
+                <ScrollView contentContainerStyle={styles.container}>
+                    <Image style={styles.logoImage}
+                        source={require('../../../static/img/UserCenter/userCenter_about_logo.png')} />
+                    <Image style={styles.centerImage}
+                        source={require('../../../static/img/UserCenter/userCenter_about_content.png')} />
+                    <Text style={styles.bottomText}>{this.state.content}</Text>
+                </ScrollView>
             </SafeAreaView>
         );
     }
@@ -57,19 +59,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: MainTheme.BackgroundColor,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
     },
 
     logoImage: {
-
+        marginTop: 20,
+        marginBottom: 20,
     },
 
     centerImage: {
-
+        marginBottom: 10,
     },
 
     bottomText: {
-
+        marginLeft: 10,
+        marginRight: 10,
+        color: MainTheme.DarkGrayColor,
+        fontSize: 12,
+        lineHeight:20,
     },
 
 });
