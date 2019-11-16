09142f944f7b5ac43f4f6f4135c0cb6cff95f331
Nail <cnnail@yahoo.com>
2019-11-16 20:18:47 +0700

    修改注销后进入注册页面时样式错误的问题

--- app/components/screen/MainScreen.js
+++ app/components/screen/MainScreen.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import {Image,StyleSheet, DeviceEventEmitter} from 'react-native';
+import React, { Component } from 'react';
+import { Image, StyleSheet, DeviceEventEmitter } from 'react-native';
 
 import CategoryScreen from '../game/CategoryScreen'
 import RegisterScreen from '../auth/RegisterScreen'
 import CustomerServiceScreen from '../home/CustomerServiceScreen'
 import LoginScreen from '../auth/LoginScreen'
 
-import {createStackNavigator, createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from "../home/HomeScreen";
 import NoticeScreen from "../home/NoticeScreen";
 import GameScreen from "../game/GameScreen";
@@ -21,7 +21,7 @@ import DepositManagerScreen from "../capital/Deposit/DepositManagerScreen"
 import DepositBankTransferScreen from "../capital/Deposit/DepositBankTransferScreen"
 import DepositPayResultScreen from "../capital/Deposit/DepositPayResultScreen"
 import MemberCenterIndexScreen from "../memberCenter/memIndex/memberCenterIndex";
-import {getStoreData} from "../../http/AsyncStorage";
+import { getStoreData } from "../../http/AsyncStorage";
 import DiscountsScreen from "../home/DiscountsScreen";
 import DiscountsDitailScreen from "../home/DiscountsDitailScreen";
 import FundRecordScreen from "../memberCenter/FundRecordScreen";
@@ -42,46 +42,53 @@ import InnerMessager from "../home/InnerMessager";
 import { theme_color } from "../../utils/AllColor";
 
 const HomeStack = createStackNavigator({
-    Home: {screen: HomeScreen,},
+    Home: { screen: HomeScreen, },
 });
 
 const CategoryStack = createStackNavigator({
-    Category: {screen: CategoryScreen},
+    Category: { screen: CategoryScreen },
 });
 
-const RegisterStack = createStackNavigator({
-    Register: {screen: RegisterScreen},
-});
+const RegisterStack = createStackNavigator(
+    {
+        Register: { screen: RegisterScreen },
+    },
+    {
+        navigationOptions: {
+            header: null,
+        },
+    }
+);
 
 const DepositStack = createStackNavigator({
-    Deposit: {screen: DepositManagerScreen},
+    Deposit: { screen: DepositManagerScreen },
 });
 
 const DiscountsScreenStack = createStackNavigator({
-    DiscountsScreen: {screen: DiscountsScreen},
+    DiscountsScreen: { screen: DiscountsScreen },
 });
 
 const LoginStack = createStackNavigator({
-    LoginService: {screen: LoginScreen},
+    LoginService: { screen: LoginScreen },
 });
 
 const MemberCenterIndexStack = createStackNavigator({
-    MemberCenterIndex: {screen: MemberCenterIndexScreen},
+    MemberCenterIndex: { screen: MemberCenterIndexScreen },
 });
 
 const LoginBeforeTabs = {
-    首页: {screen: HomeStack},
-    分类: {screen: CategoryStack},
-    注册: {screen: RegisterStack, navigationOptions: {tabBarVisible: false}},
-    优惠: {screen: DiscountsScreenStack},
-    登录: {screen: LoginStack, navigationOptions: {tabBarVisible: false}}
+    首页: { screen: HomeStack },
+    分类: { screen: CategoryStack },
+    注册: { screen: RegisterStack, navigationOptions: { tabBarVisible: false } },
+    优惠: { screen: DiscountsScreenStack },
+    登录: { screen: LoginStack, navigationOptions: { tabBarVisible: false } }
 };
 const LoginAfterTabs = {
-    首页: {screen: HomeStack},
-    分类: {screen: CategoryStack},
-    存款: {screen: DepositStack, navigationOptions: {tabBarVisible: false,color:theme_color},color: theme_color},
-    优惠: {screen: DiscountsScreenStack},
-    会员中心: {screen: MemberCenterIndexStack}
+    首页: { screen: HomeStack },
+    分类: { screen: CategoryStack },
+    存款: { screen: DepositStack, navigationOptions: { tabBarVisible: false, color: theme_color }, color: theme_color },
+    优惠: { screen: DiscountsScreenStack },
+    会员中心: { screen: MemberCenterIndexStack }
 };
 export default class MainScreen extends Component<Props> {
     constructor(props) {
@@ -92,18 +99,18 @@ export default class MainScreen extends Component<Props> {
         }
     }
 
-    componentWillMount(){
+    componentWillMount() {
         // 加载时，如果本地缓存有登录状态
         getStoreData('@loginState').then((loginInfo) => {
             if (loginInfo.isLogin) {
-                this.setState({bottomTabs: LoginAfterTabs})
+                this.setState({ bottomTabs: LoginAfterTabs })
             }
         });
         this.listener = DeviceEventEmitter.addListener('changeTabs', (val) => {
             if (val) {
-                this.setState({bottomTabs: LoginAfterTabs, initRoute:'Home'})
+                this.setState({ bottomTabs: LoginAfterTabs, initRoute: 'Home' })
             } else {
-                this.setState({bottomTabs: LoginBeforeTabs, initRoute:'LoginService'})
+                this.setState({ bottomTabs: LoginBeforeTabs, initRoute: 'LoginService' })
             }
         });
     }
@@ -118,54 +125,54 @@ export default class MainScreen extends Component<Props> {
     }
 
     TabBottom = () => (createBottomTabNavigator(
-        {...(this.state.bottomTabs)},
+        { ...(this.state.bottomTabs) },
         {
             tabBarOptions: {
                 showIcon: true,
                 resetOnBlur: true, //切换离开屏幕时，重置所有嵌套导航器的状态
                 activeTintColor: theme_color,
                 inactiveTintColor: '#a6a1aa',
-                style: {backgroundColor: 'white', height: 50},
-                labelStyle: {fontSize: 12},
+                style: { backgroundColor: 'white', height: 50 },
+                labelStyle: { fontSize: 12 },
             },
-            defaultNavigationOptions: ({navigation}) => ({
-                tabBarIcon: ({focused, tintColor}) => {
-                    const {routeName} = navigation.state;
+            defaultNavigationOptions: ({ navigation }) => ({
+                tabBarIcon: ({ focused, tintColor }) => {
+                    const { routeName } = navigation.state;
                     if (routeName === '首页') {
                         return <Image
                             source={focused ? require('../../static/img/tab_home_hover.png') :
                                 require('../../static/img/tab_home_normal.png')}
-                            style={styles.tab_icom}/>;
+                            style={styles.tab_icom} />;
                     } else if (routeName === '分类') {
                         return <Image
                             source={focused ? require('../../static/img/tab_category_hover.png') :
                                 require('../../static/img/tab_category_normal.png')}
-                            style={styles.tab_icom}/>;
+                            style={styles.tab_icom} />;
                     } else if (routeName === '注册') {
                         return <Image
                             source={focused ? require('../../static/img/tab_register.png') :
                                 require('../../static/img/tab_register.png')}
-                            style={styles.tab_icom_regiter}/>;
+                            style={styles.tab_icom_regiter} />;
                     } else if (routeName === '存款') {
                         return <Image
                             source={focused ? require('../../static/img/tab_deposit_hover.png') :
                                 require('../../static/img/tab_deposit_hover.png')}
-                            style={styles.tab_icom_regiter}/>;
+                            style={styles.tab_icom_regiter} />;
                     } else if (routeName === '优惠') {
                         return <Image
                             source={focused ? require('../../static/img/tab_cs_hover.png') :
                                 require('../../static/img/tab_cs_normal.png')}
-                            style={styles.tab_icom}/>;
+                            style={styles.tab_icom} />;
                     } else if (routeName === '登录') {
                         return <Image
                             source={focused ? require('../../static/img/tab_user_hover.png') :
                                 require('../../static/img/tab_user_normal.png')}
-                            style={styles.tab_icom}/>;
+                            style={styles.tab_icom} />;
                     } else if (routeName === '会员中心') {
                         return <Image
                             source={focused ? require('../../static/img/tab_user_hover.png') :
                                 require('../../static/img/tab_user_normal.png')}
-                            style={styles.tab_icom}/>;
+                            style={styles.tab_icom} />;
                     }
                 },
             }),
@@ -176,46 +183,46 @@ export default class MainScreen extends Component<Props> {
             Home: {
                 screen: this.TabBottom(),
             },
-            Register: {screen: RegisterStack},
-            Game: {screen: GameScreen},
-            GameList: {screen: GameListScreen},
-            SeachGameList: {screen: SeachGameListScreen},
-            AddBankScreen: {screen: AddBankScreen},
-            NewBankCardScreen: {screen: NewBankCardScreen},
-            DepositManagerScreen: {screen: DepositManagerScreen},
-            SettingCapitalPwdScreen: {screen: SettingCapitalPwdScreen},
-            PlatformTransferScreen: {screen: PlatformTransferScreen},
-            DepositBankTransferScreen: {screen: DepositBankTransferScreen},
-            WithdrawalScreen: {screen: WithdrawalScreen},
-            DepositPayResultScreen : {screen : DepositPayResultScreen},
-            LoginService: {screen: LoginScreen},
-            CustomerServiceScreen:{screen:CustomerServiceScreen},
-            DiscountDetail:{screen:DiscountsDitailScreen},
-            NoticeScreen:{screen:NoticeScreen},
-            CommonWebviewScreen:{screen:CommonWebviewScreen},
-            SecurityManagerScreen : {screen : SecurityManagerScreen},
-            ChangeLoginPwdScreen : {screen : ChangeLoginPwdScreen},
-            ChangeCapitalPwdScreen : {screen : ChangeCapitalPwdScreen},
-            BindPhoneNumScreen : {screen : BindPhoneNumScreen},
-            BankCardInfoScreen : {screen : BankCardInfoScreen},
-            FundRecord:{screen:FundRecordScreen},
-            BettingRecord:{screen:BettingRecordScreen},
-            PersonSetting:{screen:PersonSetting},
-            ContactSetting:{screen:ContactSetting},
-            AssetDetailScreen:{screen:AssetDetailScreen},
+            Register: { screen: RegisterStack },
+            Game: { screen: GameScreen },
+            GameList: { screen: GameListScreen },
+            SeachGameList: { screen: SeachGameListScreen },
+            AddBankScreen: { screen: AddBankScreen },
+            NewBankCardScreen: { screen: NewBankCardScreen },
+            DepositManagerScreen: { screen: DepositManagerScreen },
+            SettingCapitalPwdScreen: { screen: SettingCapitalPwdScreen },
+            PlatformTransferScreen: { screen: PlatformTransferScreen },
+            DepositBankTransferScreen: { screen: DepositBankTransferScreen },
+            WithdrawalScreen: { screen: WithdrawalScreen },
+            DepositPayResultScreen: { screen: DepositPayResultScreen },
+            LoginService: { screen: LoginScreen },
+            CustomerServiceScreen: { screen: CustomerServiceScreen },
+            DiscountDetail: { screen: DiscountsDitailScreen },
+            NoticeScreen: { screen: NoticeScreen },
+            CommonWebviewScreen: { screen: CommonWebviewScreen },
+            SecurityManagerScreen: { screen: SecurityManagerScreen },
+            ChangeLoginPwdScreen: { screen: ChangeLoginPwdScreen },
+            ChangeCapitalPwdScreen: { screen: ChangeCapitalPwdScreen },
+            BindPhoneNumScreen: { screen: BindPhoneNumScreen },
+            BankCardInfoScreen: { screen: BankCardInfoScreen },
+            FundRecord: { screen: FundRecordScreen },
+            BettingRecord: { screen: BettingRecordScreen },
+            PersonSetting: { screen: PersonSetting },
+            ContactSetting: { screen: ContactSetting },
+            AssetDetailScreen: { screen: AssetDetailScreen },
             HelpScreen: { screen: HelpScreen },
-            AboutPage: { screen: AboutPage},
+            AboutPage: { screen: AboutPage },
             CustomerService: { screen: CustomerServiceScreen },
-            InnerMessager: { screen: InnerMessager},
+            InnerMessager: { screen: InnerMessager },
         },
         {
             initialRouteName: this.state.initRoute,
-            defaultNavigationOptions: ({navigation}) => {
+            defaultNavigationOptions: ({ navigation }) => {
                 const routeName = navigation.state.routeName;
                 if (routeName === 'Home') {
                     return {
                         header: null,
-                        headerBackTitle:null,
+                        headerBackTitle: null,
                     };
                 }
             },
@@ -224,7 +231,7 @@ export default class MainScreen extends Component<Props> {
 
     render() {
         let AppContainer = createAppContainer(this.RootStack());
-        return <AppContainer/>;
+        return <AppContainer />;
     }
 }
 
@@ -240,7 +247,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         width: 45,
         height: 45,
-        marginBottom:20
+        marginBottom: 20
     },
 });
 
