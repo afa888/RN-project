be95910c772145526bc96b483bf06ba367bf5756
Nail <cnnail@yahoo.com>
2019-11-16 16:58:11 +0700

    站内信开发

--- app/components/home/InnerMessager.js
+++ app/components/home/InnerMessager.js
@@ -4,12 +4,23 @@ import {
     Text,
     SafeAreaView,
     FlatList,
+    Image,
     RefreshControl,
+    TouchableOpacity,
     StyleSheet
 } from 'react-native';
 
+import http from "../../http/httpFetch";
+import TXTools from '../../utils/Htools';
+import TXToastManager from '../../tools/TXToastManager';
 import MainTheme from '../../utils/AllColor'
-import SlideVerify from '../../customizeview/SlideVerify';
+
+// 站内信列表url
+const INNER_MESSAGER_MESSAGE_LIST_URL = '/User/getMessageList';
+// 读取站内信详情
+const INNER_MESSAGER_READ_MESSAGE_URL = '/User/getMessageInfo';
+// 查询的天数
+const INNER_MESSAGER_DATETIME_PERIOD = 90;
 
 export default class InnerMessager extends Component<Props> {
     static navigationOptions = ({ navigation }) => {
@@ -31,20 +42,127 @@ export default class InnerMessager extends Component<Props> {
 
         this.state = {
             refreshing: false,
-            data: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            data: [],
+            curIndex: -1,
         };
     }
 
+    componentDidMount() {
+        if (this.state.data.length == 0) {
+            this.refreshData();
+        }
+    }
+
+    refreshData = () => {
+        this.setState({ refreshing: true });
+        this.loadInnerMessages();
+    }
+
+    loadInnerMessages = () => {
+        // 查询的日期周期
+        let theDay = TXTools.dateAfter(-INNER_MESSAGER_DATETIME_PERIOD);
+        let from = TXTools.formatDateToCommonString(theDay);
+        let to = TXTools.formatDateToCommonString(new Date());
+        // 接口参数（没有分页）
+        let params = {
+            bdate: from,
+            edate: to,
+        };
+        // 发送数据请求
+        http.post(INNER_MESSAGER_MESSAGE_LIST_URL, params, true, text = '加载中...')
+            .then(res => {
+                if (res.status == 10000) {
+                    this.setState({ data: res.data });
+                }
+                else {
+                    let { msg } = res;
+                    if (msg == undefined || msg === '') {
+                        msg = '未知错误，请重试！';
+                    }
+                    TXToastManager.show(msg);
+                }
+
+                this.setState({ refreshing: false });
+
+            }).catch(err => {
+                console.log(err);
+                this.setState({ refreshing: false });
+                TXToastManager.show('网络错误，请重试！');
+            })
+    }
+
+    onListItemPressed = (item, index) => {
+        // 正在被打开
+        if (index == this.state.curIndex) {
+            this.setState({ curIndex: -1 });
+        }
+        // 未被打开过（需要加载详情）
+        else if (item.isOpened == undefined || item.isOpened == false) {
+            this.readMsgDetail(item, index);
+        }
+        else { // 已被打开过
+            this.setState({ curIndex: index });
+        }
+    }
+    /**
+     * 读取站内信详情，并标记为已读
+     */
+    readMsgDetail = (item, index) => {
+        let params = { id: item.id };
+        http.post(INNER_MESSAGER_READ_MESSAGE_URL, params, true, text = '读取中...')
+            .then(res => {
+                if (res.status == 10000) {
+                    item.message = res.data.message;
+                    item.status = res.data.status;
+                    item.isOpened = true;
+                    this.setState({ curIndex: index });
+                }
+                else {
+                    let { msg } = res;
+                    if (msg == undefined || msg === '') {
+                        msg = '未知错误，请重试！';
+                    }
+                    TXToastManager.show(msg);
+                }
+            }).catch(err => {
+                console.log(err);
+                TXToastManager.show('网络错误，请重试！');
+            });
+    }
+
+    /*------------------------------  Renders  -----------------------------------*/
+
     renderItem = ({ item, index }) => {
+        // 左边的图标
+        let icon = require('../../static/img/zhannei_icon_email_pre.png');
+        let unreadIcon = require('../../static/img/zhannei_icon_email_nor.png');
+
         return (
-            <View style={{
-                alignItems:'center',
-                paddingBottom: 10,
-                paddingTop: 10,
-                backgroundColor: (index % 2 == 0) ? 'green' : 'yellow'
-            }}>
-                <Text style={{fontSize:20}}>{item}</Text>
-            </View>
+            <TouchableOpacity style={styles.listItemContainer}
+                onPress={() => this.onListItemPressed(item, index)}>
+                {/* 左边的图片 */}
+                <Image source={item.status == '0' ? unreadIcon : icon} style={styles.listItemIcon} />
+                {/* 站内信明细 */}
+                <View style={{ flex: 1 }}>
+                    {/* 顶部 */}
+                    <View style={styles.listItemTopContainer}>
+                        {/* 站内信标题 */}
+                        <Text style={item.status == '0' ? styles.listItemTitle : styles.listItemUnreadTitle}>
+                            系统信息
+                        </Text>
+                        {/* 站内信的时间戳 */}
+                        <Text style={styles.listItemDateTime}>
+                            {item.addtime}
+                        </Text>
+                    </View>
+                    {/* 站内信内容 */}
+                    <Text style={item.status == '0' ? styles.listItemContent : styles.listItemUnreadContent}
+                        numberOfLines={this.state.curIndex == index ? 0 : 1}
+                    >
+                        {item.message}
+                    </Text>
+                </View>
+            </TouchableOpacity>
         );
     }
 
@@ -70,61 +188,108 @@ export default class InnerMessager extends Component<Props> {
 
     renderFooter = () => {
         return (
-            <View></View>
+            <View>
+                {this.renderSeparator()}
+                <View style={styles.listTail}>
+                    <Text style={styles.listTailText}> 没有更多数据了 </Text>
+                </View>
+            </View>
         );
     }
 
     renderEmptyComponent = () => {
         return (
-            <View style={{justifyContent: 'center',alignItems:'center'}}>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
                 <Text>暂无数据</Text>
             </View>
         )
-    }
-
-    refreshData = () => {
-
-    }
-
-    loadMoreData = () => {
-
     }
 
     render() {
         return (
             <SafeAreaView style={{ flex: 1 }}>
                 <FlatList
-                    style={{ ...styles.listStyle, flex:2 }}
+                    style={styles.listStyle}
                     numColumns={1}
                     data={this.state.data}
-                    keyExtractor={item => item}//这里要是使用重复的key出现莫名其妙的错误
+                    keyExtractor={item => item.addtime}//这里要是使用重复的key出现莫名其妙的错误
                     ListFooterComponent={this.renderFooter}//尾巴
                     enableEmptySections={true}//数据可以为空
                     renderItem={this.renderItem}
                     onEndReachedThreshold={0.2}//执行上啦的时候10%执行
-                    onEndReached={this.loadMoreData}
                     ListEmptyComponent={this.renderEmptyComponent()}
                     ItemSeparatorComponent={this.renderSeparator} // 分割线
                     refreshControl={this.renderRefreshControl()}
                 />
-                <View style={{
-                    flex: 1,
-                    backgroundColor: MainTheme.LightGrayColor,
-                    justifyContent: 'flex-start',
-                    alignItems: 'center',
-                    marginTop:20,
-                }}>
-                    <SlideVerify />
-                </View>
             </SafeAreaView>
         );
     }
-
 }
 
 const styles = StyleSheet.create({
     listStyle: {
         backgroundColor: MainTheme.BackgroundColor,
         flex: 5,
-    }
+    },
+
+    listItemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingBottom: 15,
+        paddingTop: 15,
+        marginLeft: 10,
+        marginRight: 10,
+        backgroundColor: MainTheme.BackgroundColor,
+    },
+
+    listItemIcon: {
+        marginLeft: 15,
+        marginRight: 15,
+        alignSelf: 'flex-start',
+    },
+
+    listItemTopContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+
+    listItemTitle: {
+        fontSize: 15,
+        fontWeight:'bold',
+        color: MainTheme.DarkGrayColor,
+    },
+
+    listItemUnreadTitle: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: MainTheme.GrayColor,
+    },
+
+    listItemDateTime: {
+        fontSize: 10,
+        color: MainTheme.GrayColor,
+    },
+
+    listItemContent: {
+        fontSize: 12,
+        color: MainTheme.DarkGrayColor,
+    },
+
+    listItemUnreadContent: {
+        fontSize: 12,
+        color: MainTheme.GrayColor,
+    },
+
+    listTail: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+    },
+
+    listTailText: {
+        color: MainTheme.GrayColor,
+        textAlign: 'center',
+        fontSize: 10
+    },
 });
--- app/utils/Htools.js
+++ app/utils/Htools.js
@@ -8,29 +8,61 @@ import { Alert } from 'react-native';
  */
 export const transferMoneyFamat = (money) => {
 	let content = String(money);
-    if (content.indexOf('--') != -1 || content.indexOf('维护中') != -1 || content.indexOf('加载中...') != -1) {
-    	return content;
-    }
+	if (content.indexOf('--') != -1 || content.indexOf('维护中') != -1 || content.indexOf('加载中...') != -1) {
+		return content;
+	}
 
-    if (Number(content) > 0) {
-    	
-    	if (content.indexOf('.') != -1) {
-    		//有小数点
-    		let pos = content.indexOf('.');
-    		let pre = content.substring(0,pos);
-    		if (pre.length == 1) {
-    			//整数部分只有一位数
-    			content = '' + content; 
-    		}else {
-    			content = content.substring(0,pos + 3);
-    		}
-    		return content;
-    	}else {
-    		return content;
-    	}
-    }else {
-    	return '0.00';
-    }
+	if (Number(content) > 0) {
+
+		if (content.indexOf('.') != -1) {
+			//有小数点
+			let pos = content.indexOf('.');
+			let pre = content.substring(0, pos);
+			if (pre.length == 1) {
+				//整数部分只有一位数
+				content = '' + content;
+			} else {
+				content = content.substring(0, pos + 3);
+			}
+			return content;
+		} else {
+			return content;
+		}
+	} else {
+		return '0.00';
+	}
 };
+
+
+/**
+ * 工具类
+ */
+export default class TXTools {
+	/**
+	 * 格式化日期为'yyyy-MM-dd'的字符串
+	 */
+	static formatDateToCommonString = (theDate) => {
+		let year = theDate.getFullYear();
+		let month = theDate.getMonth() + 1;
+		var day = theDate.getDate();
+
+		if (month < 10) {
+			month = '0' + month;
+		}
+		if (day < 10) {
+			day = '0' + day;
+		}
+
+		return ('' + year + '-' + month + '-' + day);
+	}
+
+	/**
+	 * 获取days天后的日期，days为负数则为今天之前日期
+	 */
+	static dateAfter=(days) => {
+		let today = new Date();
+		return new Date(today.getTime() + (24 * 60 * 60 * 1000 * days));
+	}
+}
 
 
