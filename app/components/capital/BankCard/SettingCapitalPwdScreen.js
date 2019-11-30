a7da67279b21b8322bbfbfc66d2abedfd36dffed
Nail <gavintx8@gmail.com>
2019-11-30 10:39:04 +0700

    修改提款密码设置页面的提示语

--- app/components/capital/BankCard/SettingCapitalPwdScreen.js
+++ app/components/capital/BankCard/SettingCapitalPwdScreen.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
-import {Platform,DeviceEventEmitter,TouchableOpacity, StyleSheet, Text, View, Button, Alert, Image,StatusBar} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, DeviceEventEmitter, TouchableOpacity, StyleSheet, Text, View, Button, Alert, Image, StatusBar } from 'react-native';
 import Dimensions from 'Dimensions'
 import TXInput from "../../../tools/TXInput"
 import http from '../../../http/httpFetch'
-import {mergeStoreData} from "../../../http/AsyncStorage";
+import { mergeStoreData } from "../../../http/AsyncStorage";
 import { NavigationActions } from 'react-navigation';
-import {TXAlert} from "../../../tools/TXAlert"
+import { TXAlert } from "../../../tools/TXAlert"
 import TXToastManager from "../../../tools/TXToastManager"
 import MainTheme from "../../../utils/AllColor"
-import {textThreeHightTitleColor} from "../../../utils/AllColor"
+import { textThreeHightTitleColor } from "../../../utils/AllColor"
 
 export default class SettingCapitalPwdScreen extends Component<Props> {
 
@@ -24,49 +24,49 @@ export default class SettingCapitalPwdScreen extends Component<Props> {
                 <View />
             )
         };
-      };
+    };
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {password:'',rePassword:''};
+        this.state = { password: '', rePassword: '' };
     }
 
     _onChange = (label, value) => {
         this.setState({ [label]: value });
     };
 
-    _onSettingPWD (){
+    _onSettingPWD() {
         if (this.state.password.length == 0) {
             TXToastManager.show('请输入新密码');
-        }else if(this.state.password.length !=4){
+        } else if (this.state.password.length != 4) {
             TXToastManager.show('请输入4位新密码');
         }
         else if (this.state.rePassword.length == 0) {
             TXToastManager.show('请输入确认密码');
-        }else if(this.state.rePassword.length !=4){
+        } else if (this.state.rePassword.length != 4) {
             TXToastManager.show('请输入4位确认密码');
-        }else if(this.state.password != this.state.rePassword){
+        } else if (this.state.password != this.state.rePassword) {
             TXToastManager.show('新密码跟确认密码不相符!');
-        }else {
+        } else {
             console.log('验证完成，调用接口');
-            let params = {npassword:this.state.password,renpassword:this.state.rePassword};
-            http.post('User/changeQkpwd', params,true).then((res) => {
+            let params = { npassword: this.state.password, renpassword: this.state.rePassword };
+            http.post('User/changeQkpwd', params, true).then((res) => {
                 if (res.status === 10000) {
                     TXToastManager.show('提款密码设置成功');
                     mergeStoreData('userInfoState',
                         {
-                        settedqkpwd: true,
-                        bankList:[]
-                    }).then((result) => {
-                        DeviceEventEmitter.emit('bindSuccess','提款密码设置完成');
-                        this.props.navigation.dispatch(NavigationActions.back());
+                            settedqkpwd: true,
+                            bankList: []
+                        }).then((result) => {
+                            DeviceEventEmitter.emit('bindSuccess', '提款密码设置完成');
+                            this.props.navigation.dispatch(NavigationActions.back());
 
 
-                    });
-                }else {
+                        });
+                } else {
                     if (!res) {
                         TXAlert('网络异常，请稍后重试');
-                    }else {
+                    } else {
                         TXAlert(res.msg);
                     }
                 }
@@ -78,17 +78,17 @@ export default class SettingCapitalPwdScreen extends Component<Props> {
 
     render() {
         return (
-            <View style={{height:Dimensions.get('window').height,alignItems: 'center',backgroundColor:MainTheme.backgroundViewColor}}>
-                <View style={{paddingTop:10,height:2,width:Dimensions.get('window').width}}>
+            <View style={{ height: Dimensions.get('window').height, alignItems: 'center', backgroundColor: MainTheme.backgroundViewColor }}>
+                <View style={{ paddingTop: 10, height: 2, width: Dimensions.get('window').width }}>
 
                 </View>
-                <TXInput label="新密码" forbiddenDot={true} keyboardType = 'numeric'  secureTextEntry={true} maxLength={4} placeholder="密码必须为4位数字的组合" textAlign='right' onChange={(value) => this._onChange('password', value)} value={this.state.password || ''}/>
-                <TXInput label="确认新密码" forbiddenDot={true} keyboardType = 'numeric'  secureTextEntry={true} maxLength={4} placeholder="请再次填写新密码" textAlign='right' onChange={(value) => this._onChange('rePassword', value)} value={this.state.rePassword || ''}/>
-                <View style={{paddingLeft:14,paddingTop:15,flexDirection: 'row',alignItems: 'center',justifyContent:'flex-start',height:30,width:Dimensions.get('window').width}}>
-                    <Text style={{fontSize:10,color:textThreeHightTitleColor}}>注:提款密码限制为</Text>
-                    <Text style={{fontSize:10,color:MainTheme.tipsSpecialTextColor}}>4位数字</Text>
+                <TXInput label="新密码" forbiddenDot={true} keyboardType='numeric' secureTextEntry={true} maxLength={4} placeholder="密码必须为4位数字" textAlign='right' onChange={(value) => this._onChange('password', value)} value={this.state.password || ''} />
+                <TXInput label="确认新密码" forbiddenDot={true} keyboardType='numeric' secureTextEntry={true} maxLength={4} placeholder="请再次填写新密码" textAlign='right' onChange={(value) => this._onChange('rePassword', value)} value={this.state.rePassword || ''} />
+                <View style={{ paddingLeft: 14, paddingTop: 15, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', height: 30, width: Dimensions.get('window').width }}>
+                    <Text style={{ fontSize: 10, color: textThreeHightTitleColor }}>注:提款密码限制为</Text>
+                    <Text style={{ fontSize: 10, color: MainTheme.tipsSpecialTextColor }}>4位数字</Text>
                 </View>
-                {MainTheme.renderCommonSubmitButton(this._onSettingPWD.bind(this),'设置')}
+                {MainTheme.renderCommonSubmitButton(this._onSettingPWD.bind(this), '设置')}
             </View>
         );
     }
@@ -97,6 +97,6 @@ export default class SettingCapitalPwdScreen extends Component<Props> {
 const styles = StyleSheet.create({
     Iuput_stype: {
         height: 100,
-        backgroundColor:'#ffffff'
+        backgroundColor: '#ffffff'
     },
 });
